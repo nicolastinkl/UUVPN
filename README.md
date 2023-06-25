31b3e4fc11792c93d3fa2dadcbfdaab3aac5d096
TK <TK@gmail.com>
2023-06-25 11:41:40 +0800

    Commit code. Update time: 2023-06-25

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # UUVPN
 基于Flutter开发的VPN客户端(ios/android)，自主设计，精美UI,优化VPN速度，完全开源。
+
+
+![](screenshots/Snipaste_2023-06-25_11-38-47.png)
---
+++ git.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+# 遍历目录及子目录下所有超过100MB大小的文件
+find . -type f -size +100M | while read file; do
+  # 获取文件名并检查是否已经存在于.gitignore文件中
+  filename="${file##*/}"
+  if ! grep "$filename" .gitignore > /dev/null 2>&1 ; then
+    # 如果文件名不存在于.gitignore文件中，则添加到末尾
+    echo "$filename" >> .gitignore
+  fi
+done
+
+
+git add .
+date +%F > timefile
+currentTime=$(<timefile)
+git commit -m "Commit code. Update time: $currentTime "
+#git push
