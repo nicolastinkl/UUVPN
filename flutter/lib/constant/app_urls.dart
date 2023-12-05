314b2ba45e0c45e5737ee2229c086bcf77bcf960
Zeus <nicolastinkl@gmail.com>
2023-12-05 14:46:51 +0800

    111
    111

--- flutter/lib/constant/app_urls.dart
+++ flutter/lib/constant/app_urls.dart
@@ -1,5 +1,5 @@
 class AppUrls {
-  static const String baseUrl = "https://kuyun888.com";
+  static const String baseUrl = "https://uu.lanmao123.com";
   static const String baseApiUrl = '$baseUrl/api/v1'; // 基础接口地址
 
   static const String login = '$baseApiUrl/passport/auth/login';
